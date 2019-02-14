bf7796f35201a335eff94582a108562334f46f55
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-14 06:37:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(blockRange) {
   return function drivingRange(start, end) {
      let distance = Math.abs(parseInt(start) - parseInt(end));
      if (distance > blockRange) {
-       return `${distance} blocks out of range`
+       return `${distance - blockRange} blocks out of range`
      } else {
        return `within range by ${distance}`
      }
